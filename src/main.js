7e372f1c657a9901c934e691dd0965a6ee4f0591
Bilal <bilal.a.khurshid@gmail.com>
2024-08-30 16:49:10 -0500

    added bootstrap and some functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { redirectToAuthCodeFlow, getAccessToken } from './auth.js';
 import { fetchProfile, fetchTopArtists } from './api.js';
 import { populateUI, populateUI2 } from './ui.js';
+import './scss/styles.scss'
 
 const clientId = "ec8cb7b304fa4772868545d97cc74fbf";
 
