0147be377e4aa6665925e3068d96ea36a7ea4af2
bilalkhurshid1 <bilal.khurshid@ahead.com>
2024-09-25 10:27:46 -0500

    fixing some bootstrap vite stuff

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(
+  {
+    build: {
+      rollupOptions: {
+        input: {
+          main: './src/index.html',
+          dashboard: './src/dashboard.html',
+        }
+      }
+    },
     esbuild: {
       supported: {
         'top-level-await': true //browsers can handle top-level-await features
